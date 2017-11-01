350b5d5a50d11d7c506a4afa4d55fbb6384c2af0
junzhezhang <zhangjunzhetom@gmail.com>
2017-11-01 16:28:44 +0800

    add mem pfile

--- memory2.cc
+++ memory2.cc
@@ -96,7 +96,7 @@ void CnMemPool::Malloc(void **ptr, const size_t size) {
   fstream file("memInfo.text", ios::in|ios::out|ios::app);
   int64_t now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
   file<<"Malloc "<<*ptr<<' '<<size<<' '<<now<<endl;
-  FILE *pfile =fopen("cnmemMemoryState.log",'a');
+  FILE *pfile =fopen("cnmemMemoryState.log","a");
   cnmemPrintMemoryState(pfile,NULL);
   fclose(pfile);
 }
@@ -109,7 +109,7 @@ void CnMemPool::Free(void *ptr) {
   fstream file("memInfo.text", ios::in|ios::out|ios::app);
   int64_t now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
   file<<"Free "<<ptr<<' '<<now<<endl;
-  FILE *pfile =fopen("cnmemMemoryState.log",'a');
+  FILE *pfile =fopen("cnmemMemoryState.log","a");
   cnmemPrintMemoryState(pfile,NULL);
   fclose(pfile);
 }
